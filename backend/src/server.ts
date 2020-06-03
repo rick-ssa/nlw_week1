33c989c2c644ae0f4ecd90556f4f1118c2a1956c
rick-ssa <rick_ssa@hotmail.com>
2020-06-02 21:40:04 -0300

    database created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes';
 import createDb from './database/create';
 
-createDb.initiateDataBase('database.sqlite')
+createDb.initiateDataBase('database.sqlite');
 
 const app = express();
 
