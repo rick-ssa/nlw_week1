b3aade26ef1cf17416e6b89e79f6d7d0f31847bb
rick-ssa <rick_ssa@hotmail.com>
2020-06-04 00:16:46 -0300

    added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import itemsRoutes from './routes/itemsRoutes';
 import pointsRoutes from './routes/pointsRoutes';
 import createDb from './database/create';
-import path from 'path'
+import path from 'path';
+import cors from 'cors';
 
 createDb.initiateDataBase('database.sqlite');
 
 const app = express();
 
+app.use(cors())
 app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')))
 app.use(express.json())
 app.use('/items',itemsRoutes)
